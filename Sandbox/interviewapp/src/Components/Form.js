2f088d7578b6a09e3dcf8d06cf104d605ab218a7
AaronDski <aarondski123@gmail.com>
2022-03-14 16:46:39 -0600

    added id generator

--- Sandbox/interviewapp/src/Components/Form.js
+++ Sandbox/interviewapp/src/Components/Form.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function Form({dishName, setDishName, dishArr, setDishArr}) {
+
+    const onInputChange = (e) => {
+        setDishName(e.target.value);
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        setDishArr([...dishName, {id: uuidv4(), title: dishName }])
+        setDishName("");
+    };
+
     return(
         <div>
-            <form>
+            <form onSubmit={onFormSubmit} >
                 <input 
                 type='text' 
-                placeholder="Enter Dish Name Here..." />
+                placeholder="Enter Dish Name Here..." 
+                value={dishName}
+                onChange={onInputChange}
+                />
                 <button type="submit" >Add</button>
 
 
